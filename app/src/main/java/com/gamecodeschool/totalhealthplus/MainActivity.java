bf3d8a7f5fa9c7fd2554b69ae17545f42b78a8f7
unknown <m47hanson47@gmail.com>
2024-02-23 11:01:00 -0500

    Joseph added some stuff to test the food db using DBBrowser

--- app/src/main/java/com/gamecodeschool/totalhealthplus/MainActivity.java
+++ app/src/main/java/com/gamecodeschool/totalhealthplus/MainActivity.java
@@ -3,6 +3,7 @@ package com.gamecodeschool.totalhealthplus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,8 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_view);
         databaseHelper = new DatabaseHelper(this);
-        //databaseHelper.insertExercise("running", 67);
+//        use db browser to view data but runnig again creates duplicates in db
+//        databaseHelper.insertExercise("running", 67);
+        databaseHelper.insertFood("Hardboiled Egg","Protein", 70, 50);
+
+        result = databaseHelper.selectFoods();
         Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+        Log.d("RESULT WAS: ", result);
     }
 
 
