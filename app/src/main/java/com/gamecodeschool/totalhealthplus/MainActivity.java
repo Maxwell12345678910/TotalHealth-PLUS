b0696ad5666b24c79ea139ac325dfd5c45d7e5ab
qiutong03 <muubidesu@gmail.com>
2024-02-23 10:38:02 -0500

    first commit login UI skeleton

--- app/src/main/java/com/gamecodeschool/totalhealthplus/MainActivity.java
+++ app/src/main/java/com/gamecodeschool/totalhealthplus/MainActivity.java
@@ -5,21 +5,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
-    DatabaseHelper databaseHelper;
-    String result = "";
+    private EditText usernameInputEDT, passwordInputEDT, firstNameInputEDT, lastNameInputEDT, ageInputEDT, weightInputEDT, heightInputEDT;
+    private Button createUserButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_view);
-        databaseHelper = new DatabaseHelper(this);
-        //databaseHelper.insertExercise("running", 67);
-        Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.testloginpage);
+        DatabaseHelper databaseHelper = new DatabaseHelper(this);
+
+        usernameInputEDT = (EditText) findViewById(R.id.usernameInput);
+        passwordInputEDT = (EditText) findViewById(R.id.passwordInput);
+        firstNameInputEDT = (EditText) findViewById(R.id.firstnameInput);
+        lastNameInputEDT = (EditText) findViewById(R.id.lastnameInput);
+        ageInputEDT = (EditText) findViewById(R.id.ageInput);
+        weightInputEDT = (EditText) findViewById(R.id.weightInput);
+        heightInputEDT = (EditText) findViewById(R.id.heightInput);
+
+        createUserButton = (Button) findViewById(R.id.button);
+
+        createUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usernameInput = usernameInputEDT.getText().toString();
+                String passwordInput = passwordInputEDT.getText().toString();
+                String firstNameInput = firstNameInputEDT.getText().toString();
+                String lastNameInput = lastNameInputEDT.getText().toString();
+
+                String ageInputString = ageInputEDT.getText().toString();
+                int ageInput = Integer.parseInt(ageInputString);
+                String weightInputString = weightInputEDT.getText().toString();
+                int weightInput = Integer.parseInt(weightInputString);
+                String heightInputString = heightInputEDT.getText().toString();
+                int heightInput = Integer.parseInt(heightInputString);
+
+                databaseHelper.insertUser(usernameInput, passwordInput, firstNameInput, lastNameInput, ageInput, weightInput, heightInput);
+            }
+        });
     }
-
-
-
-
 }
