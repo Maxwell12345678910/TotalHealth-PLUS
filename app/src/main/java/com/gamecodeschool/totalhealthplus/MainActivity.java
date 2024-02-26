c4d1afac508c1a59e4291825b161b4e7b06332c2
Joe <jgoudreau0120@gmail.com>
2024-02-26 11:01:57 -0500

    dbpush

--- app/src/main/java/com/gamecodeschool/totalhealthplus/MainActivity.java
+++ app/src/main/java/com/gamecodeschool/totalhealthplus/MainActivity.java
@@ -3,24 +3,22 @@ package com.gamecodeschool.totalhealthplus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper databaseHelper;
     String result = "";
+
+    private EditText usernameInputEDT, passwordInputEDT, firstNameInputEDT, lastNameInputEDT, ageInputEDT, weightInputEDT, heightInputEDT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_view);
         databaseHelper = new DatabaseHelper(this);
-        result = databaseHelper.selectUsers();
-//        databaseHelper.insertUser("username", "password", "John", "Smith",
-//                45, 72, 190);
-        Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
     }
-
 
 
 
