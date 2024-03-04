8e3e4045561cd6f41f28c886f0ceca71521937d6
Nick Rivers <nickfilmsinc@gmail.com>
2024-03-04 09:36:49 -0600

    updaetes post navbar appear/disappear

--- app/src/main/java/com/gamecodeschool/totalhealthplus/MainActivity.java
+++ app/src/main/java/com/gamecodeschool/totalhealthplus/MainActivity.java
@@ -1,20 +1,46 @@
 package com.gamecodeschool.totalhealthplus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.util.SparseArray;
+import android.view.MenuItem;
 import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper databaseHelper;
     String result = "";
+
+    private BottomNavigationView bottomNavigationView;
+    private SparseArray<Fragment> fragmentMap = new SparseArray<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_view);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener((BottomNavigationView.OnNavigationItemSelectedListener) this);
+
+        // Initialize fragments
+        fragmentMap.put(R.id.person, new FirstFragment());
+        fragmentMap.put(R.id.home, new SecondFragment());
+        fragmentMap.put(R.id.settings, new ThirdFragment());
+
+        // Set the initial fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.flFragment, fragmentMap.get(R.id.home))
+                    .commit();
+        }
+
         databaseHelper = new DatabaseHelper(this);
 //        use db browser to view data but runnig again creates duplicates in db
 //        databaseHelper.insertExercise("running", 67);
@@ -26,6 +52,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment selectedFragment = fragmentMap.get(item.getItemId());
+        if (selectedFragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.flFragment, selectedFragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
 
 
 }
