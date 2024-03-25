83b3d97a4a93112146d6ad2fc5280d1155f7afc0
unknown <m47hanson47@gmail.com>
2024-03-25 10:38:39 -0400

    renamed fragments to dashboards

--- app/src/main/java/com/gamecodeschool/totalhealthplus/SecondFragment.java
+++ app/src/main/java/com/gamecodeschool/totalhealthplus/SecondFragment.java
@@ -28,7 +28,7 @@ public class SecondFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        View view = inflater.inflate(R.layout.main_dashboard, container, false);
 
 
         //set up the progressbar
